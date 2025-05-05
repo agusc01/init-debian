41c7c6b1ba255d2103c22f9bebe93e74f5d0ce90
agus <33077305+agusc01@users.noreply.github.com>
2025-05-05 20:44:22 -0300

    Better vim installer

--- programming/vim.sh
+++ programming/vim.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 sudo apt install vim -y
-mv -vf ~/.config/vim{,.bak}
-git clone https://github.com/agusc01/vim ~/.config/vim
-dos2unix ~/.vim/plugged/\*\*
+mv -vf ~/.vim{,.bak}
+git clone https://github.com/agusc01/vim ~/.vim
+dos2unix ~/.vim/**
+
+mv -vf ~/.vimrc{,.bak}
+cat <<EOF >~/.vimrc
+source ~/.vim/settings.vim
+source ~/.vim/plugins.vim
+source ~/.vim/keybindings.vim
+EOF
+
 # :PlugInstall
+# After do :PlugInstall make another dos2unix
+# dos2unix ~/.vim/**
