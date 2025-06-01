d6bc77fca9594cceaccb85c0a1560019c6781769
agus <33077305+agusc01@users.noreply.github.com>
2025-06-02 04:01:31 +1000

    fix thunar dark theme

--- explorer-files/thunar.sh
+++ explorer-files/thunar.sh
@@ -4,7 +4,7 @@ sudo apt install thunar -y
 sudo apt install arc-theme -y
 
 mv -vpf ~/.config/gtk-3.0/settings.ini{,.bak}
-mkdir -vpf ~/.config/gtk-3.0
+mkdir -vp ~/.config/gtk-3.0
 cat <<EOF >~/.config/gtk-3.0/settings.ini
 [Settings]
 gtk-theme-name=Arc-Dark
