1b8a8e7cd6086b60c17b0e8e97fb1131ecf2c925
agus <33077305+agusc01@users.noreply.github.com>
2025-06-02 03:47:08 +1000

    Audio things (pulseaudio)

--- basic/audio.sh
+++ basic/audio.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+sudo apt install pulseaudio -y
+sudo apt install pulseaudio-utils -y
+pulseaudio --start
 sudo apt install pulsemixer -y # audio
 # sudo apt install pavucontrol -y # audio (with interface)
