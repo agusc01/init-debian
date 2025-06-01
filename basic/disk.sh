7126fec0bc5a9b4ce87daa80ef0e0d8064a6e14a
agus <33077305+agusc01@users.noreply.github.com>
2025-05-31 21:20:45 -0300

    disk things

---
+++ basic/disk.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt install gparted -y # interface (GUI)
+sudo ln -fvs /usr/sbin/gparted /usr/local/bin/gparted
+# sudo apt install parted -y # terminal
+# sudo ln -fvs /usr/sbin/parted /usr/local/bin/parted
