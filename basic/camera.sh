a7689fe0cd2de4161a1f6ec661c7b0e78d86f1f0
agus <33077305+agusc01@users.noreply.github.com>
2025-06-02 21:09:30 -0300

    update camera & screen scripts

--- basic/camera.sh
+++ basic/camera.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+sudo apt install guvcview -y # camera
 sudo apt install kmod -y
 sudo ln -fvs /usr/sbin/modprobe /usr/local/bin/modprobe
--- basic/screen.sh
+++ basic/screen.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo apt install x11-xserver-utils -y # for xrandr for screen's manipulation
+sudo apt install xfce4-screenshooter -y
