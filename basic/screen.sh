80651a4b81f22a857c73bd422d454410218bf3f3
agus <33077305+agusc01@users.noreply.github.com>
2025-06-20 00:37:51 -0300

    Change xfce4-screenshooter for flameshot

--- basic/screen.sh
+++ basic/screen.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 sudo apt install x11-xserver-utils -y # for xrandr for screen's manipulation
-sudo apt install xfce4-screenshooter -y
+sudo apt install flameshot -y         # screenshot
+
+# This is for rofi launcher
+sudo sed -i 's|^Exec=flameshot$|Exec=flameshot gui|' /usr/share/applications/org.flameshot.Flameshot.desktop
