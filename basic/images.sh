39681ac9954ad0a9103c7f7430fe7976733da924
agus <33077305+agusc01@users.noreply.github.com>
2025-06-20 00:36:42 -0300

    Update basic/images.sh (+pqiv,+gpick,-viewnior)

--- basic/images.sh
+++ basic/images.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt install feh -y      # image viewer (set the background image in dwm)
-sudo apt install viewnior -y # image viewer
-# sudo apt install gimp -y        # edit image
+sudo apt install feh -y     # image viewer (set the background image in dwm)
+sudo apt install pqiv -y    # image viewer
 sudo apt install mtpaint -y # edit image
+sudo apt install gpick -y   # an advanced color picker
+# sudo apt install gimp -y  # edit image
