041e901038a20c2a4e850ca3db1541b687fe3a4d
agus <33077305+agusc01@users.noreply.github.com>
2025-06-19 22:25:15 -0300

    Change from kolourpaint to mtpaint

--- basic/images.sh
+++ basic/images.sh
@@ -3,4 +3,4 @@
 sudo apt install feh -y      # image viewer (set the background image in dwm)
 sudo apt install viewnior -y # image viewer
 # sudo apt install gimp -y        # edit image
-sudo apt install kolourpaint -y # edit image
+sudo apt install mtpaint -y # edit image
