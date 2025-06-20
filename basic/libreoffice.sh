9acb7e1faa7a656a2e6e31b35cf572b7caf483a2
agus <33077305+agusc01@users.noreply.github.com>
2025-06-20 20:26:39 -0300

    Fix libreoffice

--- basic/libreoffice.sh
+++ basic/libreoffice.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-cd ~/Downloads
-sudo wget libreoffice_25.2.3_Linux_x86-64_deb.tar.gz https://es.libreoffice.org/colabora/dl/deb-x86_64/25.2.3/es/LibreOffice_25.2.3_Linux_x86-64_deb.tar.gz
-sudo tar -xzf libreoffice_25.2.3_Linux_x86-64_deb.tar.gz
-cd LibreOffice_25.2.3_Linux_x86-64_deb/DEBS
-sudo dpkg -i *.deb
-cd ~/Downloads
-sudo rm -rfv LibreOffice_25*
+sudo apt install libreoffice -y
