c95fa5e8030c6a9a2d6ab849e2f8a6e90587638b
agus <33077305+agusc01@users.noreply.github.com>
2025-06-21 22:19:17 -0300

    Add browser-sync & typescript-language-server

--- programming/nodejs-npm.sh
+++ programming/nodejs-npm.sh
@@ -3,5 +3,7 @@
 sudo apt install npm -y
 sudo apt install nodejs -y
 sudo npm install -g typescript
+sudo npm install -g typescript-language-server
 sudo npm install -g typescript-formatter
 sudo npm install -g sass
+sudo npm install -g browser-sync
