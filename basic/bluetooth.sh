53fb710d45d9e1290f302f97a6fac31ec550e1b2
agus <33077305+agusc01@users.noreply.github.com>
2025-06-21 22:19:39 -0300

    Add blueman

--- basic/bluetooth.sh
+++ basic/bluetooth.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt install bluez -y # bluetoothctl to use in the terminal
-# sudo apt install blueman -y # control bluetooth (with interface)
+sudo apt install bluez -y   # bluetoothctl to use in the terminal
+sudo apt install blueman -y # control bluetooth (with interface)
