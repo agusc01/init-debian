2df97e18ef5aac4db98fab1db09992613817d123
agus <33077305+agusc01@users.noreply.github.com>
2025-06-23 19:20:40 -0300

    update virtualbox

--- programming/virtualbox.sh
+++ programming/virtualbox.sh
@@ -3,3 +3,8 @@
 cd ~/Downloads
 sudo wget -O virtual-box.deb https://download.virtualbox.org/virtualbox/7.1.8/virtualbox-7.1_7.1.8-168469~Debian~bookworm_amd64.deb
 sudo dpkg -i virtual-box.deb && sudo rm -vf ~/Downloads/virtual-box.deb
+
+sudo apt update
+sudo apt install linux-headers-$(uname -r)
+sudo apt install build-essential dkms -y
+sudo /sbin/vboxconfig
