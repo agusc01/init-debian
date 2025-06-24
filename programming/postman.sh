4153637e51454babca18f38565fe4fc6b0024993
agus <33077305+agusc01@users.noreply.github.com>
2025-06-24 20:40:48 -0300

    update postman

--- programming/postman.sh
+++ programming/postman.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 cd ~/Downloads
-sudo wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
+sudo wget -O postman.tar.gz https://dl.pstmn.io/download/latest/linux64
 sudo tar -xzf postman.tar.gz
 sudo mv -v Postman/ /opt/postman
 sudo ln -fvs /opt/postman/Postman /usr/bin/postman && sudo rm -rfv ~/Downloads/postman**
+
+sudo bash -c 'cat <<EOF >/usr/share/applications/postman.desktop
+[Desktop Entry]
+Version=1.0
+Name=Postman
+Exec=/opt/postman/app/postman
+Icon=/opt/postman/app/icons/icon_128x128.png
+Type=Application
+Categories=Development;
+Terminal=false
+EOF'
