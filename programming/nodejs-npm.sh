1df0c7d94741cd08a8326fbff76c4909691ebdc7
agus <33077305+agusc01@users.noreply.github.com>
2025-08-24 16:29:53 -0300

    update npm

--- programming/nodejs-npm.sh
+++ programming/nodejs-npm.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+# sudo apt install npm -y
+# sudo apt install nodejs -y
 
-sudo apt install npm -y
-sudo apt install nodejs -y
+cd /tmp
+wget https://nodejs.org/dist/v22.18.0/node-v22.18.0-linux-x64.tar.xz
+sudo tar -xvf node-v22.18.0-linux-x64.tar.xz
+sudo mv node-v22.18.0-linux-x64 /usr/local/nodejs
+sudo ln -fvs /usr/local/nodejs/bin/node /usr/local/bin/
+sudo ln -fvs /usr/local/nodejs/bin/npx /usr/local/bin/
+sudo ln -fvs /usr/local/nodejs/bin/npm /usr/local/bin/
+sudo ln -fvs /usr/local/nodejs/bin/corepack /usr/local/bin/
+
 sudo npm install -g typescript
 sudo npm install -g typescript-language-server
 sudo npm install -g typescript-formatter
