8ad498d083c7e01fd1960e62988fad74218f483b
agus <33077305+agusc01@users.noreply.github.com>
2025-08-31 23:45:09 -0300

    update backgrounds

--- backgrounds/background.sh
+++ backgrounds/background.sh
@@ -2,3 +2,4 @@
 
 mkdir -vp ~/Pictures/backgrounds/
 cp -v ~/.config/init-debian/backgrounds/*.jpg ~/Pictures/backgrounds/
+sed -i 's|^output \* bg .*|output * bg ~/Pictures/backgrounds/debian-matrix.jpg fill|' ~/.config/sway/config
