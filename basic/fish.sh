ebb67a35677aa3936ede154b70cdb1a96b8f5c94
developer <dev@ghostman.ghostman.org>
2025-09-21 05:46:42 +1000

    update fish.sh

--- basic/fish.sh
+++ basic/fish.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-sudo apt install fish -y
-chsh -s /usr/bin/fish
+if command -v fish &> /dev/null; then
+    echo 'fish is already installed !'
+else
+    sudo apt install fish -y
+    # chsh -s /usr/bin/fish
 
-mv -vf ~/.config/fish{,.bak}
-git clone https://github.com/agusc01/fish ~/.config/fish
-rm -rfv ~/.config/fish/fish_variables || true
-mv -v ~/.config/fish.bak/fish_variables ~/.config/fish/fish_variables
-source ~/.config/fish/config.fish
+    mv -vf ~/.config/fish{,.bak}
+    git clone https://github.com/agusc01/fish ~/.config/fish
+    rm -rfv ~/.config/fish/fish_variables || true
+    mv -v ~/.config/fish.bak/fish_variables ~/.config/fish/fish_variables
+    source ~/.config/fish/config.fish
+fi
