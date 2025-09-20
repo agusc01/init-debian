30403a588a55662f74d931e785a258c33e8d6d33
developer <dev@ghostman.ghostman.org>
2025-09-21 05:49:14 +1000

    update background.sh

--- backgrounds/background.sh
+++ backgrounds/background.sh
@@ -2,4 +2,11 @@
 
 mkdir -vp ~/Pictures/backgrounds/
 cp -v ~/.config/init-debian/backgrounds/*.jpg ~/Pictures/backgrounds/
-sed -i 's|^output \* bg .*|output * bg ~/Pictures/backgrounds/debian-matrix.jpg fill|' ~/.config/sway/config
+
+if grep -q '^output' ~/.config/sway/config; then
+    sed -i 's|^output \* bg .*|output * bg ~/Pictures/backgrounds/banana-black.jpg fill|' ~/.config/sway/config
+else
+    echo "output * bg ~/Pictures/backgrounds/banana-black.jpg fill" >>  ~/.config/sway/config
+fi
+
+
