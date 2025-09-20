9bbf1f62a98fb26ec36093907aa97ab93e0bff1c
developer <dev@ghostman.ghostman.org>
2025-09-21 05:49:36 +1000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,54 +1,60 @@
 #!/bin/bash
 
-# cd ~/.config/init-debian/ && find . -type f -name '*.sh' | grep -v install.sh | grep -v bash | grep -v core | grep -v updates | grep -v dbar | sort | awk 'BEGIN {print "./basic/core.sh"} {print} END {print "./basic/uninstall.sh\n./basic/bash.sh"}' | copyme
+~/.config/init-debian/wm/sway.sh
+~/.config/init-debian/terminal/alacratty.sh
+# ~/.config/init-debian/terminal/konsole.sh
+~/.config/init-debian/backgrounds/background.sh
 
-./basic/core.sh
-./backgrounds/background.sh
-./basic/audio.sh
-./basic/bluetooth.sh
-./basic/camera.sh
-./basic/disk.sh
-./basic/fish.sh
-./basic/images.sh
-./basic/keyboard.sh
-./basic/libreoffice.sh
-./basic/maths.sh
-./basic/resources.sh
-./basic/screen.sh
-./basic/video.sh
-./basic/wifi.sh
-./browsers/brave.sh
-./browsers/chrome.sh
-./browsers/firefox-esr.sh
-./browsers/librewolf.sh
-./browsers/qutebrowser.sh
-# ./browsers/surf.sh
-./browsers/vieb.sh
-./explorer-files/cfiles.sh
-./explorer-files/qtfm.sh
-./explorer-files/ranger.sh
-# ./explorer-files/thunar.sh
-./fonts/font.sh
-./programming/filezilla.sh
-./programming/lazy-docker.sh
-./programming/lazy-git.sh
-./programming/lazy-npm.sh
-./programming/mongo.sh
-./programming/nodejs-npm.sh
-./programming/nvim.sh
-./programming/obsidian.sh
-./programming/php.sh
-./programming/postman.sh
-./programming/raspberry-pi-imager.sh
-./programming/sublime-text.sh
-./programming/vagrant.sh
-./programming/vim.sh
-./programming/virtualbox.sh
-./programming/vscode.sh
-# ./suckless/dmenu.sh
-./suckless/dshortcuts.sh
-./suckless/dwm.sh
-./suckless/slock.sh
-./suckless/st.sh
-./basic/uninstall.sh
-./basic/bash.sh
+~/.config/init-debian/basic/audio.sh
+~/.config/init-debian/basic/bluetooth.sh
+~/.config/init-debian/basic/camera.sh
+~/.config/init-debian/basic/compiler.sh
+~/.config/init-debian/basic/core.sh
+~/.config/init-debian/basic/disk.sh
+~/.config/init-debian/basic/images.sh
+~/.config/init-debian/basic/keyboard.sh
+~/.config/init-debian/basic/libreoffice.sh
+~/.config/init-debian/basic/maths.sh
+~/.config/init-debian/basic/pdf.sh
+~/.config/init-debian/basic/resources.sh
+~/.config/init-debian/basic/screen.sh
+~/.config/init-debian/basic/time.sh
+~/.config/init-debian/basic/video.sh
+~/.config/init-debian/basic/wifi.sh
+
+# ~/.config/init-debian/explorer-files/cfiles.sh
+# ~/.config/init-debian/explorer-files/dolphin.sh
+~/.config/init-debian/explorer-files/lf.sh
+# ~/.config/init-debian/explorer-files/qtfm.sh
+~/.config/init-debian/explorer-files/ranger.sh
+~/.config/init-debian/explorer-files/thunar.sh
+~/.config/init-debian/explorer-files/yazi.sh
+
+~/.config/init-debian/browsers/brave.sh
+~/.config/init-debian/browsers/chrome.sh
+~/.config/init-debian/browsers/firefox-esr.sh
+~/.config/init-debian/browsers/librewolf.sh
+~/.config/init-debian/browsers/qutebrowser.sh
+# ~/.config/init-debian/browsers/vieb.sh # does not work for swayWM
+
+~/.config/init-debian/programming/filezilla.sh
+~/.config/init-debian/programming/lazy-docker.sh
+~/.config/init-debian/programming/lazy-git.sh
+~/.config/init-debian/programming/lazy-npm.sh
+~/.config/init-debian/programming/mongo.sh
+~/.config/init-debian/programming/nodejs-npm.sh
+~/.config/init-debian/programming/nvim.sh
+~/.config/init-debian/programming/obsidian.sh
+~/.config/init-debian/programming/php.sh
+~/.config/init-debian/programming/postman.sh
+~/.config/init-debian/programming/raspberry-pi-imager.sh
+~/.config/init-debian/programming/rust.sh
+~/.config/init-debian/programming/sublime-text.sh
+~/.config/init-debian/programming/vagrant.sh
+~/.config/init-debian/programming/vim.sh
+~/.config/init-debian/programming/virtualbox.sh
+~/.config/init-debian/programming/vscode.sh
+
+~/.config/init-debian/basic/bash.sh
+~/.config/init-debian/basic/fish.sh
+~/.config/init-debian/basic/uninstall.sh
