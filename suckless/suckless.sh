b4edb0e72a86b0cec645a60c9a149aa7dab54e69
agus <33077305+agusc01@users.noreply.github.com>
2025-10-05 07:56:08 +1100

    suckless things

---
+++ suckless/suckless.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt install libx11-dev -y
+sudo apt install libxft-dev -y
+sudo apt install libxinerama-dev -y
+sudo apt install libxext-dev -y   # rounded corner patch
+sudo apt install libimlib2-dev -y # preview patch
+sudo apt install libxcomposite-dev -y
+sudo apt install libxrandr-dev -y
+sudo apt install libxrender-dev -y
+sudo apt install libxext-dev -y
