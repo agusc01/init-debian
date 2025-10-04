4be95de09333db41ddd3b2da0f26d8c323f4341f
agus <33077305+agusc01@users.noreply.github.com>
2025-10-05 07:55:53 +1100

    st.sh

--- terminal/st.sh
+++ terminal/st.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ~/.config/init-debian/basic/compiler.sh
+~/.config/init-debian/suckless/suckless.sh
 sudo apt install git -y
 sudo apt install picom -y
 sudo mv -vf ~/.config/st{,.bak}
@@ -9,3 +10,17 @@ cd ~/.config/st
 sudo touch config.h
 sudo rm -vf config.h
 sudo make clean install
+
+sudo bash -c 'cat <<EOF >/usr/share/applications/st.desktop
+[Desktop Entry]
+Version=1.0
+Name=ST
+Comment=Simple Terminal
+Exec=st
+Icon=~/.config/st/st.png
+Terminal=false
+Type=Application
+Categories=Terminal;Utility;
+EOF'
+
+sudo chmod +x /usr/share/applications/st.desktop
