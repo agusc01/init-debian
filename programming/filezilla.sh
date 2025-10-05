2966b6e4d7186772479d3399d5854afa2008269b
agus <33077305+agusc01@users.noreply.github.com>
2025-10-06 09:30:36 +1100

    update filezilla

--- programming/filezilla.sh
+++ programming/filezilla.sh
@@ -7,19 +7,20 @@ else
     sudo apt install wget -y
     sudo apt install tar -y
     cd /tmp
-    sudo wget -O filezilla.tar.xz "https://dl2.cdn.filezilla-project.org/client/FileZilla_3.69.3_x86_64-linux-gnu.tar.xz?h=1UOoX_Lj7xb6SdBapVHUMg&x=1757211721"
+    sudo wget -O filezilla.tar.xz "https://dl1.cdn.filezilla-project.org/client/FileZilla_3.69.3_x86_64-linux-gnu.tar.xz?h=i51DvTzhrRnxpbqnkDZFCA&x=1759706521"
+
     sudo tar -xvf filezilla.tar.xz
     sudo mv -v FileZilla3 /opt/filezilla
     sudo ln -fvs /opt/filezilla/bin/filezilla /usr/local/bin/filezilla
 
     sudo mv -vf /usr/share/applications/filezilla.desktop{,.bak}
     sudo bash -c 'cat <<EOF > /usr/share/applications/filezilla.desktop
-    [Desktop Entry]
-    Name=FileZilla
-    Exec=/opt/filezilla/bin/filezilla
-    Icon=/opt/filezilla/share/icons/hicolor/32x32/apps/filezilla.png
-    Type=Application
-    Categories=Network;FileTransfer;
-    EOF'
+[Desktop Entry]
+Name=FileZilla
+Exec=/opt/filezilla/bin/filezilla
+Icon=/opt/filezilla/share/icons/hicolor/32x32/apps/filezilla.png
+Type=Application
+Categories=Network;FileTransfer;
+EOF'
     # sudo chmod +x /usr/share/applications/filezilla.desktop
 fi
