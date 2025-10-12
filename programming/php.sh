0a89a86a7ee09611b715fa70b9b214ec1c8e92f1
agus <33077305+agusc01@users.noreply.github.com>
2025-10-12 15:01:04 -0300

    update php.sh

--- programming/php.sh
+++ programming/php.sh
@@ -9,9 +9,16 @@ sudo npm install -g intelephense # for nvim (lazyvim)
 sudo apt install php -y
 sudo apt install php-mysql -y
 
+sudo systemctl stop apache2
+sudo systemctl disable apache2
+sudo sed -i 's/Listen 80/Listen 81/' /etc/apache2/ports.conf
+sudo systemctl start apache2
+sudo systemctl enable apache2
+
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'.PHP_EOL; } else { echo 'Installer corrupt'.PHP_EOL; unlink('composer-setup.php'); exit(1); }"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'ed0feb545ba87161262f2d45a633e34f591ebb3381f2e0063c345ebea4d228dd0043083717770234ec00c5a9f9593792') { echo 'Installer verified'.PHP_EOL; } else { echo 'Installer corrupt'.PHP_EOL; unlink('composer-setup.php'); exit(1); }"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
 sudo mv composer.phar /usr/local/bin/composer
+
