7103c647d667558d8bcbaa33b71b1ce63bd4119a
agus <33077305+agusc01@users.noreply.github.com>
2025-10-12 15:00:49 -0300

    update background.sh (without sway things)

--- backgrounds/background.sh
+++ backgrounds/background.sh
@@ -3,10 +3,3 @@
 mkdir -vp ~/Pictures/backgrounds/
 cp -v ~/.config/init-debian/backgrounds/*.jpg ~/Pictures/backgrounds/
 
-if grep -q '^output' ~/.config/sway/config; then
-    sed -i 's|^output \* bg .*|output * bg ~/Pictures/backgrounds/banana-black.jpg fill|' ~/.config/sway/config
-else
-    echo "output * bg ~/Pictures/backgrounds/banana-black.jpg fill" >>  ~/.config/sway/config
-fi
-
-
